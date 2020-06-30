39cd12ffaca709fa1ab84b36c602d6f062765d61
Ramesh Madara <Admin@MacBook-Pro.local>
2020-07-01 01:30:10 +0530

    Updates

---
+++ precache-manifest.abce02e01bc103a44b146603a83ec005.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6e7d727fa78352e243e751c622d903",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "a2832d284d097e36fcdf",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a2832d284d097e36fcdf",
+    "url": "/contactmanager/static/js/2.483c0103.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.483c0103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3383b40103a912175d",
+    "url": "/contactmanager/static/js/main.878f6c07.chunk.js"
+  },
+  {
+    "revision": "f35700e63daa1061f73a",
+    "url": "/contactmanager/static/js/runtime-main.cb8970ef.js"
+  }
+]);
